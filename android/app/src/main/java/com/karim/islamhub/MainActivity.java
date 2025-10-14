5e41ddf94b52c1a01af265c023f4899b97d1f5bb
karim romdhane <karimromdhane7@gmail.com>
2025-10-14 20:00:44 +0200

    android added

---
+++ android/app/src/main/java/com/karim/islamhub/MainActivity.java
@@ -0,0 +1,5 @@
+package com.karim.islamhub;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.karim.islamhub',
+  appName: 'islamic-website',
+  webDir: 'dist'
+};
+
+export default config;
